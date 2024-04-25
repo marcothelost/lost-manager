7d921bc60b7bb5499b8607bedb33c1a53a7ab20b
marcothelost <marcothelost@proton.me>
2024-04-25 20:55:00 +0200

    Screen clearing and input loading improvements

--- src/Main.c
+++ src/Main.c
@@ -3,42 +3,54 @@
 #include <stdbool.h>
 #include <string.h>
 
-const char INPUT_SYMBOL = '$';
-const int STRINGS_MATCHING = 0;
+const char         INPUT_SYMBOL     = '$';
+const int          STRINGS_MATCHING = 0;
+const unsigned int BUFFER_SIZE      = 128;
 
+void clearScreen();
 void foo();
 void bar();
 
 int main()
 {
-  char buffer[64];
+  char* buffer = (char*)malloc(sizeof(char) * BUFFER_SIZE);
   bool running = true;
+
+  clearScreen();
+
   while (strcmp(buffer, "exit"))
   {
     printf("%c ", INPUT_SYMBOL);
-    scanf(" %s", &buffer);
+    fgets(buffer, BUFFER_SIZE, stdin);
+    buffer[strcspn(buffer, "\n")] = 0;
+
+    if (strcmp(buffer, "exit") == STRINGS_MATCHING)
+    {
+      continue;
+    }
 
     if (strcmp(buffer, "foo") == STRINGS_MATCHING)
     {
       foo();
-      continue;
     }
     else if (strcmp(buffer, "bar") == STRINGS_MATCHING)
     {
       bar();
-      continue;
+    }
+    else if (strcmp(buffer, "clear") == STRINGS_MATCHING)
+    {
+      clearScreen();
+    }
+    else
+    {
+      printf("Command \"%s\" does not exist!\n", buffer);
     }
   }
 
+  free(buffer);
   return EXIT_SUCCESS;
 }
 
-void foo()
-{
-  printf("Foo!\n");
-}
-
-void bar()
-{
-  printf("Bar!\n");
-}
+void clearScreen() { printf("\e[1;1H\e[2J"); }
+void foo() { printf("Foo!\n"); }
+void bar() { printf("Bar!\n"); }
